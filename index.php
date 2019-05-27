05455175ce83cc4a8d8ffe77d2d807b37c735930
unknown <mastermomad@gmail.com>
2019-05-27 15:57:25 +0800

    Initial HTML file

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php
-echo "new topic";
-echo"add new topics again";
-echo "sth new";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is my first file</title>
+</head>
+<body>
+     <h1>This is my First File</h1>
+</body>
+</html>
